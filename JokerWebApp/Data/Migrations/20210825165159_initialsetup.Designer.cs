240fb2ed1d9413971ebec7e2cd05b0ed9bfdf430
Dani-Iliev <75176422+Dani-Iliev@users.noreply.github.com>
2021-08-25 20:47:58 +0300

    Add Db

--- JokerWebApp/Data/Migrations/20210825165159_initialsetup.Designer.cs
+++ JokerWebApp/Data/Migrations/20210825165159_initialsetup.Designer.cs
@@ -4,14 +4,16 @@ using JokerWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JokerWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210825165159_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
